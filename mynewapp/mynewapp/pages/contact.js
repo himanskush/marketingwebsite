e5142dfe39f12f9e632e3280a2ed803e1e0d2b21
Rukhsar Shakir <rukhsar.shakir@indiamart.com>
2021-03-30 17:17:25 +0530

    contact us

--- mynewapp/mynewapp/pages/contact.js
+++ mynewapp/mynewapp/pages/contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 /* middleware */
@@ -11,9 +11,216 @@ import {
 
 /* components */
 import Layout from '../components/layout/Layout';
+import FormContact from '../components/form/FormContact';
+
+const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,2|3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+/* login schemas */
+const FORM_DATA_CONTACT = {
+  name: {
+    value: '',
+    label: 'Name',
+    min: 3,
+    max: 36,
+    required: true,
+    validator: {
+      regEx: /^[a-zA-Z\\s]*$/,
+      error: 'Please insert valid name',
+    },
+  },
+  email: {
+    value: '',
+    label: 'Email',
+    min: 10,
+    max: 36,
+    required: true,
+    validator: {
+      regEx: emailRegEx,
+      error: 'Please insert valid email',
+    },
+  },
+  message: {
+    value: '',
+    label: 'Message',
+    min: 10,
+    max: 100,
+    required: true,
+  },
+  contactno: {
+    value: '',
+    label: 'contactno',
+    min: 10,
+    max: 10,
+    required: true,
+    validator: {
+      regEx: /^[1-9]{1}[0-9]{9}$/,
+      error: 'Please insert valid contact number',
+    },
+  },
+};
 
 export default function Contact(props) {
   const { profile } = props;
+  const [stateFormData, setStateFormData] = useState(FORM_DATA_CONTACT);
+  const [stateFormError, setStateFormError] = useState([]);
+  const [stateFormValid, setStateFormValid] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [stateFormMessage, setStateFormMessage] = useState({});
+
+  function onChangeHandler(e) {
+    const { name, value } = e.currentTarget;
+
+    setStateFormData({
+      ...stateFormData,
+      [name]: {
+        ...stateFormData[name],
+        value,
+      },
+    });
+
+    /* validation handler */
+    validationHandler(stateFormData, e);
+  }
+
+  async function onSubmitHandler(e) {
+    e.preventDefault();
+
+    let data = { ...stateFormData };
+    /*name */
+    data = { ...data, name: data.name.value || '' };
+    /* email */
+    data = { ...data, email: data.email.value || '' };
+    /* contact no */
+    data = { ...data, contactno: data.contactno.value || '' };
+    /* message */
+    data = { ...data, message: data.message.value || '' };
+    /* validation handler */
+    const isValid = validationHandler(stateFormData);
+
+    if (isValid) {
+      // Call an external API endpoint to get posts.
+      // You can use any data fetching library
+      setLoading(!loading);
+      const loginApi = await fetch(`${baseApiUrl}/auth`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      }).catch(error => {
+        console.error('Error:', error);
+      });
+      let result = await loginApi.json();
+      if (result.success && result.token) {
+        Cookies.set('token', result.token);
+        // window.location.href = referer ? referer : "/";
+        // const pathUrl = referer ? referer.lastIndexOf("/") : "/";
+        Router.push('/');
+      } else {
+        setStateFormMessage(result);
+      }
+      setLoading(false);
+    }
+  }
+
+  function validationHandler(states, e) {
+    const input = (e && e.target.name) || '';
+    const errors = [];
+    let isValid = true;
+
+    if (input) {
+      if (states[input].required) {
+        if (!states[input].value) {
+          errors[input] = {
+            hint: `${states[e.target.name].label} required`,
+            isInvalid: true,
+          };
+          isValid = false;
+        }
+      }
+      if (
+        states[input].value &&
+        states[input].min > states[input].value.length
+      ) {
+        errors[input] = {
+          hint: `Field ${states[input].label} min ${states[input].min}`,
+          isInvalid: true,
+        };
+        isValid = false;
+      }
+      if (
+        states[input].value &&
+        states[input].max < states[input].value.length
+      ) {
+        errors[input] = {
+          hint: `Field ${states[input].label} max ${states[input].max}`,
+          isInvalid: true,
+        };
+        isValid = false;
+      }
+      if (
+        states[input].validator !== null &&
+        typeof states[input].validator === 'object'
+      ) {
+        if (
+          states[input].value &&
+          !states[input].validator.regEx.test(states[input].value)
+        ) {
+          errors[input] = {
+            hint: states[input].validator.error,
+            isInvalid: true,
+          };
+          isValid = false;
+        }
+      }
+    } else {
+      Object.entries(states).forEach(item => {
+        item.forEach(field => {
+          errors[item[0]] = '';
+          if (field.required) {
+            if (!field.value) {
+              errors[item[0]] = {
+                hint: `${field.label} required`,
+                isInvalid: true,
+              };
+              isValid = false;
+            }
+          }
+          if (field.value && field.min >= field.value.length) {
+            errors[item[0]] = {
+              hint: `Field ${field.label} min ${field.min}`,
+              isInvalid: true,
+            };
+            isValid = false;
+          }
+          if (field.value && field.max <= field.value.length) {
+            errors[item[0]] = {
+              hint: `Field ${field.label} max ${field.max}`,
+              isInvalid: true,
+            };
+            isValid = false;
+          }
+          if (field.validator !== null && typeof field.validator === 'object') {
+            if (field.value && !field.validator.regEx.test(field.value)) {
+              errors[item[0]] = {
+                hint: field.validator.error,
+                isInvalid: true,
+              };
+              isValid = false;
+            }
+          }
+        });
+      });
+    }
+    if (isValid) {
+      setStateFormValid(isValid);
+    }
+    setStateFormError({
+      ...errors,
+    });
+    return isValid;
+  }
 
   function handleOnClickLogout(e) {
     setLogout(e);
@@ -23,9 +230,23 @@ export default function Contact(props) {
     <Layout title="Contact Us Page">
       <div className="container">
         <main>
-          <h1 className="title">Contact Page</h1>
+          <h1 className="title">Contact Us</h1>
           {!profile ? (
-            <a href="/">Login to continue</a>
+            /*<a href="/">Login to continue</a>*/
+            <>
+              <div>
+                <FormContact
+                  props={{
+                    onSubmitHandler,
+                    onChangeHandler,
+                    loading,
+                    stateFormData,
+                    stateFormError,
+                    stateFormMessage,
+                  }}
+                />
+              </div>
+            </>
           ) : (
             <div>
               <div style={{ margin: '.5rem 0rem' }}>
